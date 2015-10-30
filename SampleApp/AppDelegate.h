f2fb76613e7d7852d44fd146d5548baab78358e2
Suman Raj <sumanraj.v@gmail.com>
2015-10-30 11:23:27 -0700

    Initial Commit

---
+++ SampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by Suman Raj Venkatesan on 10/30/15.
+//  Copyright © 2015 Kiip Inc.,. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Suman Raj Venkatesan on 10/30/15.
+//  Copyright © 2015 Kiip Inc.,. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
